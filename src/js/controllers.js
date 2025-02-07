63b99c4fc51d60f0e4f07ecdd38a894370ed6eb5
Hinata-AWAIISHIMA-FB <h.awaiishima@freebit.net>
2025-02-07 11:58:55 +0900

    Fixed the votesCount sum of last 50 blocks update process. Separated the votesCount update process from the voteCountChart update process.

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -362,10 +362,12 @@ netStatsApp.controller('StatsCtrl', function($scope, $filter, $localStorage, soc
 				if( !_.isEqual($scope.gasSpending, data.gasSpending) && data.gasSpending.length >= MAX_BINS )
 					$scope.gasSpending = data.gasSpending;
 
-				if( !_.isEqual($scope.voteCountChart, data.votes) && data.votes.length >= MAX_BINS ) {
+				if( !_.isEqual($scope.voteCountChart, data.votes)  && data.votes.length >= MAX_BINS ) {
 					$scope.voteCountChart = data.votes;
-					$scope.votesCount = data.votesCount;
 				}
+
+				if( !_.isEqual($scope.votesCount, data.votesCount) )
+					$scope.votesCount = data.votesCount;
 
 				if( !_.isEqual($scope.miners, data.miners) ) {
 					$scope.miners = data.miners;
